ede9ebdab24e0d3d0d099a1e1d17b526a9957a2c
Philip <legoboy1122@yahoo.com>
2017-04-08 20:34:40 -0500

    Started work on the tree placement algorithm

--- Terrain_Gen_Test/Assets/Oggal's TerrainGen/TerrainDeco.cs
+++ Terrain_Gen_Test/Assets/Oggal's TerrainGen/TerrainDeco.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class TerrainDeco : MonoBehaviour {
 
+    [SerializeField]
+    private float TreeDensity = 0.5f;
+    [SerializeField]
+    private float StoneDensity = 0.1f;
+    [SerializeField]
+    private bool Generate = false;
+
+    private bool GenTrees = false;
+    private bool GenStones = false;
+    private bool FailedTreeIsStone = false;
+
+    float World_X;
+    float World_Z;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,6 +30,20 @@ public class TerrainDeco : MonoBehaviour {
 
     public void Deco()
     {
-
+        World_X = transform.localPosition.x;
+        World_Z = transform.localPosition.z;
+        if (!Generate) return;
+        Vector3[] Verts = GetComponent<MeshFilter>().mesh.vertices;
+        if (GenTrees)
+        {
+            foreach(Vector3 v in Verts)
+            {
+                RaycastHit hitInfo;
+                if(Physics.Raycast(new Vector3(v.x, v.y + 1, v.z), Vector3.down, out hitInfo))
+                {
+                    hitInfo.normal;
+                }
+            }
+        }
     }
 }
